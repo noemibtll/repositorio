8366f408d6c11516894e4fb576822d4a2dff8ace
noemibtll <noemi.btll.55@outlook.com>
2021-07-27 19:23:00 -0500

    mi primer commit en develop

---
+++ A8 PROGRAMA 2.py
@@ -0,0 +1,9 @@
+#Realice un programa que muestre en pantalla la tabla de multiplicar de un n�mero pedido al usuario (hasta el 20 los valores que multiplica).
+
+
+    
+print ("tabla de multiplicar")
+a= int (input("ingresa un numero que quieras multiplicar")
+def tabla_multiplicar:
+    for a in range (1,21):
+        print (a)
